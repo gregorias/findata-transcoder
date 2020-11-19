1c3cd8691cf67d7d41a6aaa4fa5e380921dce53c
Grzegorz Milka <grzegorzmilka@gmail.com>
2020-11-19 22:39:02 +0100

    Add a better test

--- test/Test/Bcge/Hint.hs
+++ test/Test/Bcge/Hint.hs
@@ -7,9 +7,12 @@ import qualified Bcge.Hint       as BH
 
 tests = do
   describe "Bcge.Hint tests" $ do
-    it "Correctly parses and applies hints" $ do
-      BH.transactionTitleToHint config "TWINT 123A"
-        `shouldBe` Just (BH.TransactionHint "Twint" "Expenses:Groceries")
+    it "Correctly parses and applies first matching hint" $ do
+      BH.transactionTitleToHint config "TWINT MIGROS 123A"
+        `shouldBe` Just (BH.TransactionHint "Migros" "Expenses:Groceries")
       where
         Just config = parseMaybe BH.configParser
-          "keyword,title,counterAccount\nTWINT,Twint,Expenses:Groceries"
+          ("keyword,title,counterAccount\n"
+          ++ "MIGROS,Migros,Expenses:Groceries\n"
+          ++ "TWINT,Twint,Expenses:Other\n"
+          )
