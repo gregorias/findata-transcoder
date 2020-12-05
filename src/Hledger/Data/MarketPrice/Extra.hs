21d6b6c6684b9ef99b889baf51d2331cf07ab002
Grzegorz Milka <grzegorzmilka@gmail.com>
2020-12-05 12:38:29 +0100

    Add MarketPrice formatting

---
+++ src/Hledger/Data/MarketPrice/Extra.hs
@@ -0,0 +1,23 @@
+module Hledger.Data.MarketPrice.Extra (showMarketPrice) where
+
+import Data.Time.Format.ISO8601
+  ( FormatExtension (ExtendedFormat),
+    calendarFormat,
+    formatShow,
+  )
+import Hledger (MarketPrice (MarketPrice))
+import Text.Printf (printf)
+
+-- | Renders MarketPrice as Ledger-style text
+--
+-- @
+-- P yyyy-mm-dd from price to
+-- @
+showMarketPrice :: MarketPrice -> String
+showMarketPrice (MarketPrice day from to price) =
+  printf
+    "P %s %s %s %s\n"
+    (formatShow (calendarFormat ExtendedFormat) day)
+    from
+    (show price)
+    to
---
+++ test/Test/Hledger/Data/MarketPrice/Extra.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Hledger.Data.MarketPrice.Extra (tests) where
+
+import Data.Ratio ((%))
+import Data.Time (fromGregorian)
+import Hledger (MarketPrice (MarketPrice))
+import Hledger.Data.MarketPrice.Extra
+import Test.Hspec (describe, it, shouldBe)
+import qualified Test.Hspec as Hspec
+
+tests :: Hspec.SpecWith ()
+tests = do
+  describe "Hledger.Data.MarketPrice.Extra tests" $ do
+    it "Correctly displays a market price" $ do
+      showMarketPrice
+        ( MarketPrice
+            (fromGregorian 2020 11 26)
+            "ACWF"
+            "USD"
+            (fromRational $ 3224 % 100)
+        )
+        `shouldBe` "P 2020-11-26 ACWF 32.24 USD\n"
