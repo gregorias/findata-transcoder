fc0bd90eb8186dfdadd5a1d9a088a2a9c34bab83
Grzegorz Milka <grzegorzmilka@gmail.com>
2020-12-24 12:11:14 +0100

    Provide csvStatementToLedger with documentation

--- src/Hledupt/Degiro.hs
+++ src/Hledupt/Degiro.hs
@@ -7,7 +7,7 @@ import qualified Data.ByteString.Lazy as LBS
 import Hledupt.Degiro.Csv (parseCsvStatement)
 import Relude
 
--- |
+-- | Transforms a Degiro CSV statement into a Ledger report
 csvStatementToLedger :: LBS.ByteString -> Either String Text
 csvStatementToLedger stmtTxt = do
   _degiroCsvRecords <- parseCsvStatement stmtTxt
