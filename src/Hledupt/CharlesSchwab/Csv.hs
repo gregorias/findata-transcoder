90397a78720ef27737d3d95ab671c4723b22db7d
Grzegorz Milka <grzegorzmilka@gmail.com>
2021-02-07 13:47:47 +0100

    Implement Charles Schwab CSV parsing

--- src/Hledupt/CharlesSchwab/Csv.hs
+++ src/Hledupt/CharlesSchwab/Csv.hs
@@ -6,39 +6,141 @@ module Hledupt.CharlesSchwab.Csv (
   parseCsStatement,
 
   -- * Types
+  DollarAmount (..),
   CsCsvRecord (..),
 ) where
 
 import Control.Lens (over)
 import qualified Control.Lens as L
+import Control.Monad.Combinators (manyTill)
 import qualified Data.ByteString.Lazy as LBS
+import Data.Csv (FromNamedRecord (..), (.:))
+import qualified Data.Csv as Csv
 import Data.Decimal (Decimal)
 import qualified Data.Text as Text
-import Data.Time (Day)
+import Data.Time (Day, defaultTimeLocale)
+import Data.Time.Format (parseTimeM)
 import Data.Vector (Vector)
-import Hledupt.Data.Cash (Cash)
-import Hledupt.Data.CsvFile (CsvFile)
+import Hledupt.Data.CsvFile (CsvFile (CsvFile))
+import Hledupt.Data.MyDecimal (decimalP)
 import Relude
-import Text.Megaparsec (Parsec)
+import Text.Megaparsec (Parsec, anySingle, chunk, single)
 import qualified Text.Megaparsec as MP
+import Text.Megaparsec.Char (space)
+import Text.Megaparsec.Char.Lexer (signed)
+import Text.Megaparsec.Stream (tokensToChunk)
+
+csSimpleDayP :: Parsec Void String Day
+csSimpleDayP = do
+  content <- many anySingle
+  parseTimeM True defaultTimeLocale "%m/%d/%Y" content
+
+csAsOfDayP :: Parsec Void String Day
+csAsOfDayP = do
+  content <- manyTill anySingle (MP.single ' ')
+  void MP.takeRest
+  parseTimeM True defaultTimeLocale "%m/%d/%Y" content
+
+csDayP :: Parsec Void String Day
+csDayP = MP.try csSimpleDayP <|> csAsOfDayP
+
+newtype CsDay = CsDay
+  { unCsDay :: Day
+  }
+
+instance Csv.FromField CsDay where
+  parseField field = do
+    let (day :: Maybe Day) = MP.parseMaybe csDayP (decodeUtf8 field)
+    maybe (fail $ "Could not parse the date field: " ++ decodeUtf8 field) (return . CsDay) day
+
+-- I would not need this type with dependent types. I would just use (Cash CHF)
+newtype DollarAmount = DollarAmount
+  { unDollarAmount :: Decimal
+  }
+  deriving newtype (Eq, Ord, Show)
+
+unsignedDollarAmountP :: Parsec Void Text Decimal
+unsignedDollarAmountP = do
+  void $ single '$'
+  decimalP
+
+dollarAmountP :: Parsec Void Text DollarAmount
+dollarAmountP = DollarAmount <$> signed space unsignedDollarAmountP
+
+quantityP :: Csv.NamedRecord -> Csv.Parser (Maybe Integer)
+quantityP rec = do
+  field <- rec .: "Quantity"
+  if field == ""
+    then pure Nothing
+    else return $ readMaybe field
+
+maybeDollarAmountP :: Csv.NamedRecord -> ByteString -> Csv.Parser (Maybe DollarAmount)
+maybeDollarAmountP rec name = do
+  field <- rec .: name
+  if field == ""
+    then pure Nothing
+    else return $ MP.parseMaybe dollarAmountP field
+
+instance Csv.FromField DollarAmount where
+  parseField field = do
+    let fieldString = Text.pack $ decodeUtf8 field
+    maybe
+      (fail . Text.unpack $ "Could not parse the dollar amount: " `Text.append` fieldString)
+      return
+      (MP.parseMaybe dollarAmountP fieldString)
 
 data CsCsvRecord = CsCsvRecord
   { csDate :: !Day
   , csAction :: !Text
   , csSymbol :: !Text
   , csDescription :: !Text
-  , csQuantity :: !Integer
-  , csPrice :: !Decimal
-  , csFees :: !Cash
-  , csAmount :: !Cash
+  , csQuantity :: !(Maybe Integer)
+  , csPrice :: !(Maybe DollarAmount)
+  , csFees :: !(Maybe DollarAmount)
+  , csAmount :: !DollarAmount
   }
   deriving stock (Eq, Ord, Show)
 
+instance FromNamedRecord CsCsvRecord where
+  parseNamedRecord rec =
+    CsCsvRecord
+      <$> (unCsDay <$> rec .: "Date")
+      <*> rec .: "Action"
+      <*> rec .: "Symbol"
+      <*> rec .: "Description"
+      <*> quantityP rec
+      <*> maybeDollarAmountP rec "Price"
+      <*> maybeDollarAmountP rec "Fees & Comm"
+      <*> rec .: "Amount"
+
+line :: Parsec Void LBS.ByteString LBS.ByteString
+line = do
+  (content, eol) <- MP.manyTill_ anySingle (chunk "\n")
+  return $ tokensToChunk (Proxy :: Proxy LBS.ByteString) content `LBS.append` eol
+
+dropFirstLine :: Parsec Void LBS.ByteString ()
+dropFirstLine = do
+  void $ manyTill anySingle (chunk "\n")
+
+lastLine :: Parsec Void LBS.ByteString ()
+lastLine = do
+  void $ chunk "Transactions Total"
+  void MP.takeRest
+
 csStatementToCsvContentP :: Parsec Void LBS.ByteString (CsvFile LBS.ByteString)
-csStatementToCsvContentP = fail "Unimplemented"
+csStatementToCsvContentP = do
+  dropFirstLine
+  csvLines <- manyTill line (MP.try lastLine)
+  return $ CsvFile (LBS.concat csvLines)
 
 parseCsCsv :: CsvFile LBS.ByteString -> Either Text (Vector CsCsvRecord)
-parseCsCsv _ = Left "Unimplemented"
+parseCsCsv (CsvFile input) =
+  snd
+    <$> L.over
+      L._Left
+      Text.pack
+      ( Csv.decodeByName input
+      )
 
 parseCsStatement :: LBS.ByteString -> Either Text (Vector CsCsvRecord)
 parseCsStatement stmt = do
--- test/Test/Hledupt/CharlesSchwab/Csv.hs
+++ test/Test/Hledupt/CharlesSchwab/Csv.hs
@@ -1,12 +1,56 @@
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Test.Hledupt.CharlesSchwab.Csv (
   tests,
 ) where
 
+import Data.Ratio ((%))
+import Data.Time (fromGregorian)
+import Hledupt.CharlesSchwab.Csv (CsCsvRecord (CsCsvRecord), DollarAmount (..), parseCsStatement)
 import Relude
-import Test.Hspec (SpecWith, describe)
+import Test.Hspec (SpecWith, describe, it)
+import Test.Hspec.Expectations.Pretty (shouldBe)
 
 tests :: SpecWith ()
 tests = do
   describe "Hledupt.CharlesSchwab.Csv" $ do
     describe "parseCsStatement" $ do
-      return ()
+      it "parses a Charles Schwab Statement" $ do
+        let csv =
+              "Transactions  for account johndoe  XXXX-0123 as of 01/31/2021 04:06:58 ET\n\
+              \\"Date\",\"Action\",\"Symbol\",\"Description\",\"Quantity\",\"Price\",\"Fees & Comm\",\"Amount\",\n\
+              \\"01/28/2021\",\"Credit Interest\",\"\",\"SCHWAB1 INT 12/30-01/27\",\"\",\"\",\"\",\"$0.19\",\n\
+              \\"01/19/2021\",\"Wire Funds\",\"\",\"WIRED FUNDS DISBURSED\",\"\",\"\",\"\",\"-$123.45\",\n\
+              \\"12/31/2020 as of 12/29/2020\",\"Sell\",\"GOOG\",\"ALPHABET INC. CLASS C\",\"5\",\"$1765.2706\",\"$0.20\",\"$8826.15\",\n\
+              \Transactions Total,\"\",\"\",\"\",\"\",\"\",\"\",$0.19"
+        parseCsStatement csv
+          `shouldBe` Right
+            [ CsCsvRecord
+                (fromGregorian 2021 1 28)
+                "Credit Interest"
+                ""
+                "SCHWAB1 INT 12/30-01/27"
+                Nothing
+                Nothing
+                Nothing
+                (DollarAmount (fromRational $ 19 % 100))
+            , CsCsvRecord
+                (fromGregorian 2021 1 19)
+                "Wire Funds"
+                ""
+                "WIRED FUNDS DISBURSED"
+                Nothing
+                Nothing
+                Nothing
+                (DollarAmount (fromRational $ -12345 % 100))
+            , CsCsvRecord
+                (fromGregorian 2020 12 31)
+                "Sell"
+                "GOOG"
+                "ALPHABET INC. CLASS C"
+                (Just 5)
+                (Just $ DollarAmount (fromRational $ 17652706 % 10000))
+                (Just $ DollarAmount (fromRational $ 20 % 100))
+                (DollarAmount (fromRational $ 882615 % 100))
+            ]
