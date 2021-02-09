082711d6d016ae917fdcace1d27f9e9fae1371b1
Grzegorz Milka <grzegorzmilka@gmail.com>
2021-02-09 09:31:00 +0100

    Add a basic test

--- test/Test/Hledupt/CharlesSchwab/Ledger.hs
+++ test/Test/Hledupt/CharlesSchwab/Ledger.hs
@@ -1,12 +1,36 @@
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Test.Hledupt.CharlesSchwab.Ledger (
   tests,
 ) where
 
+import Data.Ratio ((%))
+import Data.Time (fromGregorian)
+import Hledupt.CharlesSchwab.Csv (
+  CsCsvRecord (CsCsvRecord),
+  DollarAmount (..),
+ )
+import Hledupt.CharlesSchwab.Ledger (csvToLedger)
 import Relude
-import Test.Hspec (SpecWith, describe)
+import Test.Hspec (SpecWith, describe, it)
+import Test.Hspec.Expectations.Pretty (shouldBe)
 
 tests :: SpecWith ()
 tests = do
   describe "Hledupt.CharlesSchwab.Ledger" $ do
-    describe "parseStatement" $ do
-      return ()
+    describe "csvToLedger" $ do
+      it "transforms CSV entries" $ do
+        let entries =
+              [ CsCsvRecord
+                  (fromGregorian 2021 1 19)
+                  "Wire Funds"
+                  ""
+                  "WIRED FUNDS DISBURSED"
+                  Nothing
+                  Nothing
+                  Nothing
+                  (DollarAmount (fromRational $ -12345 % 100))
+              ]
+        csvToLedger entries
+          `shouldBe` Left "Unimplemented"
