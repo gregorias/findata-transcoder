c82e95825386de2a66af70134c9454d7e4ebae5d
Grzegorz Milka <grzegorzmilka@gmail.com>
2021-03-07 12:17:27 +0100

    Implement parsing deductions

--- src/Hledupt/GPayslip.hs
+++ src/Hledupt/GPayslip.hs
@@ -1,36 +1,52 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wno-deprecations #-}
+{-# OPTIONS_GHC -Wno-unused-matches #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
 -- | This module parses a text dump from a Google Payslip and outputs a ledger.
 module Hledupt.GPayslip (
   payslipTextToLedger,
   Payslip (..),
-  SalaryElements (..),
   Deductions (..),
   PayslipLedgerConfig (..),
   parsePayslip,
   payslipToTransaction,
 ) where
 
-import Control.Lens ((%~))
+import Control.Lens ((%~), (^.))
 import qualified Control.Lens as L
 import Data.Decimal (Decimal)
 import qualified Data.Text as Text
 import Data.Time (Day, defaultTimeLocale, parseTimeM)
+import Data.Time.Calendar (toGregorian)
 import Hledger (Status (Cleared, Pending), Transaction, missingamt, post, transaction)
 import Hledger.Data.Extra (makeCurrencyAmount)
 import Hledger.Data.Lens (pMaybeAmount, pStatus, tDescription)
 import Hledupt.Data.Currency (Currency (CHF))
 import Hledupt.Data.LedgerReport (LedgerReport (..))
 import Hledupt.Data.MyDecimal (
-  ChunkSepFormat (ChunkSep),
+  ChunkSepFormat (ChunkSep, NoChunkSep),
   DecimalFormat (..),
-  DecimalFractionFormat (TwoDigitDecimalFraction),
+  DecimalFractionFormat (
+    OptionalUnlimitedDecimalFraction,
+    TwoDigitDecimalFraction
+  ),
   decimalP,
  )
 import Relude
-import Text.Megaparsec (Parsec, anySingle, count, errorBundlePretty, manyTill, manyTill_, match, parse, try)
+import Text.Megaparsec (
+  Parsec,
+  anySingle,
+  choice,
+  count,
+  errorBundlePretty,
+  manyTill,
+  manyTill_,
+  match,
+  parse,
+  skipMany,
+  try,
+ )
 import Text.Megaparsec.Char (
   char,
   digitChar,
@@ -54,23 +70,11 @@ defaultPayslipLedgerConfig =
         "Assets:Illiquid:AXA Wintherthur Pension Fund"
     }
 
-data PayslipDuo = PayslipDuo
-  { payslipDuoDate :: !Day
-  , payslipDuoSalaryElements :: !SalaryElements
-  , payslipDuoDeductions :: ()
-  , payslipDuoTotal :: !Decimal
-  }
-  deriving stock (Show, Eq)
-
 data Payslip = Payslip
   { payslipDate :: !Day
+  , payslipMonthlySalaryTotal :: !Decimal
+  , payslipDeductions :: !Deductions
   , payslipTotal :: !Decimal
-  }
-  deriving stock (Show, Eq)
-
-data SalaryElements = SalaryElements
-  { salaryElementsMonthlySalary :: !Decimal
-  , salaryElementsTotal :: !Decimal
   }
   deriving stock (Show, Eq)
 
@@ -104,6 +108,85 @@ dateLineP = do
     "%d.%m.%Y"
     (Text.unpack dateString)
 
+nameAndAmountLineP :: Text -> Parsec Void Text Decimal
+nameAndAmountLineP name = do
+  try $ void $ string name
+  space1
+  amount <- decimalP (DecimalFormat (ChunkSep '\'') (Just TwoDigitDecimalFraction))
+  void newline
+  return amount
+
+-- | Parses a payslip line that in addition to an amount contains the reference and rate.
+-- Returns the amount.
+nameReferenceRateAmountLineP :: Parsec Void Text () -> Parsec Void Text Decimal
+nameReferenceRateAmountLineP nameP = do
+  try nameP
+  space1
+  _rate <- decimalP (DecimalFormat NoChunkSep (Just OptionalUnlimitedDecimalFraction))
+  space1
+  amount <- decimalP (DecimalFormat (ChunkSep '\'') (Just TwoDigitDecimalFraction))
+  _reference <- decimalP (DecimalFormat (ChunkSep '\'') (Just TwoDigitDecimalFraction))
+  void newline
+  return amount
+
+subTotalP :: Parsec Void Text Decimal
+subTotalP = nameAndAmountLineP "Total"
+
+salaryLineP :: Parsec Void Text ()
+salaryLineP = do
+  void $ try $ string "SALARY"
+  void $ string " ELEMENTS AmountRate (%)\n"
+
+deductionsLineP :: Parsec Void Text ()
+deductionsLineP = do
+  void $ try $ string "DEDUCTIONS"
+  void $ string " Reference Rate (%)\n"
+
+socialSecurityP :: Parsec Void Text Decimal
+socialSecurityP =
+  nameReferenceRateAmountLineP
+    (void $ string "Swiss Social Security (AHV/IV/EO)")
+
+unemploymentInsuranceP :: Parsec Void Text Decimal
+unemploymentInsuranceP = do
+  contribution0 <- nameReferenceRateAmountLineP unemploymentInsuranceNameP
+  void $ optional newline
+  contribution1 <- nameReferenceRateAmountLineP unemploymentInsuranceNameP
+  return $ contribution0 + contribution1
+ where
+  unemploymentInsuranceNameP :: Parsec Void Text ()
+  unemploymentInsuranceNameP = do
+    void $
+      choice
+        [ string "Unemployment Insurance compl."
+        , string "Unemployment Insurance"
+        ]
+
+taxAtSourceP :: Parsec Void Text Decimal
+taxAtSourceP = nameReferenceRateAmountLineP (void $ string "Tax at Source")
+
+pensionFundP :: Parsec Void Text Decimal
+pensionFundP = nameAndAmountLineP "Pension Fund"
+
+deductionsP :: Parsec Void Text Deductions
+deductionsP = do
+  deductionsLineP
+  (_, socialSecurity) <- manyTill_ newline socialSecurityP
+  skipMany newline
+  unemploymentInsurance <- unemploymentInsuranceP
+  (_, pensionFund) <- manyTill_ newline pensionFundP
+  skipMany newline
+  taxAtSource <- taxAtSourceP
+  (_, total) <- manyTill_ newline subTotalP
+  return $
+    Deductions
+      { deductionsSwissSocialSecurity = socialSecurity
+      , deductionsUnemploymentInsurance = unemploymentInsurance
+      , deductionsPensionFund = pensionFund
+      , deductionsTaxAtSource = taxAtSource
+      , deductionsTotal = total
+      }
+
 mainTotalP :: Parsec Void Text Decimal
 mainTotalP = do
   try $ void $ string "TOTAL"
@@ -115,9 +198,13 @@ mainTotalP = do
 payslipP :: Parsec Void Text Payslip
 payslipP = do
   (_, day) <- manyTill_ anyLineP dateLineP
+  void $ manyTill anyLineP salaryLineP
+  (_, salaryTotal) <- manyTill_ anyLineP subTotalP
+  void $ many newline
+  deductions <- deductionsP
   (_, mainTotal) <- manyTill_ anyLineP mainTotalP
   void $ many anyLineP
-  return $ Payslip day mainTotal
+  return $ Payslip day salaryTotal deductions mainTotal
 
 prependErrorMessage :: Text -> Either Text a -> Either Text a
 prependErrorMessage err = L._Left %~ (errln `Text.append`)
@@ -134,8 +221,19 @@ parsePayslip payslip = prepareErrMsg parsedPayslip
 
 payslipToTransaction :: PayslipLedgerConfig -> Payslip -> Transaction
 payslipToTransaction
-  (PayslipLedgerConfig bankAccount _secondPillarAccount)
-  (Payslip day mainTotal) =
+  (PayslipLedgerConfig bankAccount secondPillarAccount)
+  ( Payslip
+      day
+      salaryTotal
+      Deductions
+        { deductionsSwissSocialSecurity = socialSecurity
+        , deductionsUnemploymentInsurance = unemploymentInsurance
+        , deductionsPensionFund = pensionFund
+        , deductionsTaxAtSource = taxAtSource
+        , deductionsTotal = total
+        }
+      mainTotal
+    ) =
     transaction
       day
       [ post bankAccount missingamt
@@ -143,9 +241,24 @@ payslipToTransaction
             . L.set pMaybeAmount (Just $ makeCurrencyAmount CHF mainTotal)
       , post "Income:Google" missingamt
           & L.set pStatus Cleared
-            . L.set pMaybeAmount (Just $ makeCurrencyAmount CHF (- mainTotal))
+            . L.set pMaybeAmount (Just $ makeCurrencyAmount CHF (- salaryTotal))
+      , post (statePrefix `Text.append` "Mandatory Contributions:Social Security") missingamt
+          & L.set pStatus Cleared
+            . L.set pMaybeAmount (Just $ makeCurrencyAmount CHF socialSecurity)
+      , post (statePrefix `Text.append` "Mandatory Contributions:Unemployment Insurance") missingamt
+          & L.set pStatus Cleared
+            . L.set pMaybeAmount (Just $ makeCurrencyAmount CHF unemploymentInsurance)
+      , post secondPillarAccount missingamt
+          & L.set pStatus Cleared
+            . L.set pMaybeAmount (Just $ makeCurrencyAmount CHF pensionFund)
+      , post (statePrefix `Text.append` "Withholding Tax:Total") missingamt
+          & L.set pStatus Cleared
+            . L.set pMaybeAmount (Just $ makeCurrencyAmount CHF taxAtSource)
       ]
       & L.set tDescription "Google Salary"
+   where
+    year :: Integer = toGregorian day ^. L._1
+    statePrefix = "State:" `Text.append` show year `Text.append` ":"
 
 payslipToLedger :: PayslipLedgerConfig -> Payslip -> LedgerReport
 payslipToLedger payslipLedgerConfig payslip =
--- test/Test/Hledupt/GPayslip.hs
+++ test/Test/Hledupt/GPayslip.hs
@@ -8,6 +8,7 @@ import qualified Data.Text.IO as Text
 import Data.Time (fromGregorian)
 import Hledger.Read.TestUtils (parseTransactionUnsafe)
 import Hledupt.GPayslip (
+  Deductions (..),
   Payslip (..),
   PayslipLedgerConfig (PayslipLedgerConfig),
   parsePayslip,
@@ -29,14 +30,39 @@ tests = do
           `shouldBe` Right
             ( Payslip
                 (fromGregorian 2020 1 24)
+                12345.60
+                ( Deductions
+                    { deductionsSwissSocialSecurity = 1234.50
+                    , deductionsUnemploymentInsurance = 123.85 + 123.55
+                    , deductionsPensionFund = 123.30
+                    , deductionsTaxAtSource = 1234.75
+                    , deductionsTotal = 10000.95
+                    }
+                )
                 11234.65
             )
     describe "payslipToTransaction" $ do
       it "Transforms a payslip" $ do
-        let config = PayslipLedgerConfig "Bank" "SecondFillar"
-        let payslip = Payslip (fromGregorian 2020 1 24) 11234.65
+        let config = PayslipLedgerConfig "Bank" "SecondPillar"
+        let payslip =
+              Payslip
+                (fromGregorian 2020 1 24)
+                12345.60
+                ( Deductions
+                    { deductionsSwissSocialSecurity = 1234.50
+                    , deductionsUnemploymentInsurance = 123.85 + 123.55
+                    , deductionsPensionFund = 123.30
+                    , deductionsTaxAtSource = 1234.75
+                    , deductionsTotal = 10000.95
+                    }
+                )
+                11234.65
         payslipToTransaction config payslip
           `shouldBe` parseTransactionUnsafe
             "2020/01/24 Google Salary\n\
             \  ! Bank  11234.65 CHF\n\
-            \  * Income:Google  -11234.65 CHF\n"
+            \  * Income:Google  -12345.60 CHF\n\
+            \  * State:2020:Mandatory Contributions:Social Security  1234.50 CHF\n\
+            \  * State:2020:Mandatory Contributions:Unemployment Insurance  247.40 CHF\n\
+            \  * SecondPillar  123.30 CHF\n\
+            \  * State:2020:Withholding Tax:Total  1234.75 CHF"
