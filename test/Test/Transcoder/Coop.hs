e4feff3e1b9108e1815ff117e2a07aaf5c366bc2
Grzegorz Milka <grzegorzmilka@gmail.com>
2023-06-23 20:47:49 +0200

    chore: update stack to 21.0

--- test/Test/Transcoder/Coop.hs
+++ test/Test/Transcoder/Coop.hs
@@ -46,10 +46,10 @@ tests = do
                 [trimming|
                 2021/04/09 * Coop
                   ! Assets:Liquid:BCGE  -0.17 CHF
+                  Expenses:Groceries:Ready Meals  0.03 CHF
                   Expenses:Haushalt               0.11 CHF
                   Expenses:Gesundheit             0.02 CHF
-                  Expenses:Groceries:Chewing Gum  0.01 CHF
-                  Expenses:Groceries:Ready Meals  0.03 CHF|]
+                  Expenses:Groceries:Chewing Gum  0.01 CHF|]
 
         Coop.receiptToLedger Coop.emptyConfig coop `shouldBe` Right expectedTr
 
@@ -60,8 +60,8 @@ tests = do
                 [trimming|
                 2021/04/09 * Coop
                   ! Assets:Liquid:BCGE  -44.35 CHF
-                  Expenses:Groceries     37.35 CHF
                   Expenses:Haushalt       9.40 CHF
+                  Expenses:Groceries     37.35 CHF
                   Expenses:Other         -2.40 CHF|]
         Coop.receiptToLedger Coop.emptyConfig coop `shouldBe` Right expectedTr
 
