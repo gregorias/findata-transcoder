4b15559b3bed46f5d1ec707bf190f3d57f53101f
Grzegorz Milka <grzegorzmilka@gmail.com>
2023-07-27 11:43:02 +0200

    fix: remove shouldSatisfy

--- test/Test/Data/Time/Calendar/Extra.hs
+++ test/Test/Data/Time/Calendar/Extra.hs
@@ -3,7 +3,7 @@ module Test.Data.Time.Calendar.Extra (tests) where
 import Data.Time.Calendar.Extra (monthP)
 import Data.Void (Void)
 import Test.Hspec (Spec, describe, it)
-import Test.Hspec.Expectations.Pretty (shouldBe, shouldSatisfy)
+import Test.Hspec.Expectations.Pretty (shouldBe)
 import Text.Megaparsec (parseMaybe)
 import Prelude
 
