d0eec5862b544ba7462c45d06160b15a4f06af5b
Grzegorz Milka <grzegorzmilka@gmail.com>
2023-11-15 08:31:25 +0100

    feat: add nestP

--- src/Text/Megaparsec/Extra.hs
+++ src/Text/Megaparsec/Extra.hs
@@ -2,6 +2,11 @@
 
 module Text.Megaparsec.Extra (
   parsePretty,
+
+  -- * Nested parsing
+  nestP,
+  ToNestedParser (..),
+  ParsecWithSource (..),
 ) where
 
 import Data.Either.Extra (mapLeft)
@@ -9,6 +14,7 @@ import Language.Haskell.TH.Lift.Generics (genericLiftTypedCompat)
 import Language.Haskell.TH.Syntax (Lift (..))
 import Relude
 import Text.Megaparsec (
+  MonadParsec,
   Parsec,
   ShowErrorComponent,
   TraversableStream,
@@ -20,6 +26,37 @@ import Text.Megaparsec qualified as MP
 
 instance Lift MP.Pos where
   liftTyped = genericLiftTypedCompat
+
+-- A utility class for 'nestP'.
+class ToNestedParser s r p | p -> r, p -> s where
+  toNestedParser :: p -> (s -> Either Text r)
+
+instance ToNestedParser s r (s -> Either Text r) where
+  toNestedParser = id
+
+data ParsecWithSource e s r = ParsecWithSource
+  { pwsSource :: !Text
+  , pwsParsec :: !(Parsec e s r)
+  }
+
+instance (VisualStream s, TraversableStream s, ShowErrorComponent e) => ToNestedParser s r (ParsecWithSource e s r) where
+  toNestedParser (ParsecWithSource{pwsSource, pwsParsec}) = parsePretty pwsParsec pwsSource
+
+-- | Parses with a decoder and its follow up parser while maintaining correct cursor offsets.
+nestP ::
+  ( MonadParsec e s m
+  , MonadFail m
+  ) =>
+  m a ->
+  (a -> Either Text b) ->
+  m b
+nestP decodeP f = do
+  originalPos <- MP.getOffset
+  decoded <- decodeP
+  either
+    (MP.region (MP.setErrorOffset originalPos) . fail . toString)
+    pure
+    (f decoded)
 
 -- | Parses input and provides a pretty error message.
 parsePretty ::
