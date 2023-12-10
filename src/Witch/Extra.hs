778560cbf5080961655a9effd34a99c3f4125e05
Grzegorz Milka <grzegorzmilka@gmail.com>
2023-12-10 12:22:36 +0100

    feat(witch): prettify the nested exception

--- src/Witch/Extra.hs
+++ src/Witch/Extra.hs
@@ -8,7 +8,7 @@ module Witch.Extra (
 import Data.Either.Combinators (mapLeft)
 import Prettyprinter (Pretty (..), vsep, (<+>))
 import Prettyprinter qualified as P
-import Prettyprinter.Extra (PrettyException, unPrettyException)
+import Prettyprinter.Extra (PrettyException (..), unPrettyException)
 import Relude
 import Text.Nicify (nicify)
 import Witch (TryFrom (..), TryFromException (..), maybeTryFrom)
@@ -17,18 +17,21 @@ instance TryFrom (Maybe a) a where
   tryFrom = maybeTryFrom id
 
 -- | Nests a TryFromException inside another a potentially wider one.
+--
+-- Transforms the nested exception into a PrettyException for pretty multi-line
+-- printing. I don't need it for anything else. Can't pretty print it otherwise.
 nestException ::
-  (Show source, Typeable source, Typeable target) =>
+  (Show source) =>
   source' ->
   TryFromException source target ->
   TryFromException source' target'
-nestException newSource e = TryFromException newSource (Just (SomeException e))
+nestException newSource e = TryFromException newSource (Just (SomeException $ PrettyException $ pretty e))
 
 -- | Lift nestException to the Either monad used by TryFrom.
 --
 -- Name inspired by Aeson's eitherDecodeFoo family of functions.
 eitherNestException ::
-  (Show source, Typeable source, Typeable target) =>
+  (Show source) =>
   source' ->
   Either (TryFromException source target) b ->
   Either (TryFromException source' target') b
--- test/Test/Witch/Extra.hs
+++ test/Test/Witch/Extra.hs
@@ -9,7 +9,7 @@ import Test.Hspec (describe, it)
 import Test.Hspec qualified as Hspec
 import Test.Hspec.Expectations.Pretty (shouldBe)
 import Witch (TryFromException (..))
-import Witch.Extra ()
+import Witch.Extra qualified as Witch
 
 data AdtSource = AdtSource {asA :: !Text, asB :: !Text}
   deriving stock (Show, Typeable)
@@ -18,19 +18,19 @@ tests :: Hspec.SpecWith ()
 tests = do
   describe "Witch.Extra" $ do
     describe "Pretty TryFromException" $ do
-      it "shows a pretty exception in multiple lines" $ do
-        let base = PrettyException "foo"
-        let firstLayer = TryFromException @Text "inner source" (Just (SomeException base))
-        let firstLayerPretty = PrettyException $ pretty firstLayer
-        let secondLayer = TryFromException @Text "outer source" (Just (SomeException firstLayerPretty))
-        void $ putDocW 5 (pretty secondLayer)
-        show (pretty secondLayer)
-          `shouldBe` [trimming|
-              TryFromException
-              source: "outer source"
-              cause: TryFromException
-                     source: "inner source"
-                     cause: foo|]
+      describe "nestException" $ do
+        it "shows a pretty exception in multiple lines" $ do
+          let base = PrettyException "foo"
+          let firstLayer = TryFromException @Text "inner source" (Just (SomeException base))
+          let secondLayer = Witch.nestException @Text "outer source" firstLayer
+          void $ putDocW 5 (pretty secondLayer)
+          show (pretty secondLayer)
+            `shouldBe` [trimming|
+                TryFromException
+                source: "outer source"
+                cause: TryFromException
+                       source: "inner source"
+                       cause: foo|]
 
       it "pretty prints the source" $ do
         let ex = TryFromException (AdtSource "A" "B") Nothing
