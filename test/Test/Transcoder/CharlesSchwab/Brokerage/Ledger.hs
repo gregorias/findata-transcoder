15be16e564e4a24055e1e2cea3ab1bb49c7d7d1d
Grzegorz Milka <grzegorzmilka@gmail.com>
2024-08-05 09:26:54 +0200

    refactor(cs): clean up

--- test/Test/Transcoder/CharlesSchwab/Brokerage/Ledger.hs
+++ test/Test/Transcoder/CharlesSchwab/Brokerage/Ledger.hs
@@ -2,7 +2,6 @@ module Test.Transcoder.CharlesSchwab.Brokerage.Ledger (
   tests,
 ) where
 
-import Data.Ratio ((%))
 import Data.Time (fromGregorian)
 import Hledger.Read.TestUtils (transactionsQQ)
 import Relude
@@ -21,94 +20,85 @@ tests = do
   describe "Transcoder.CharlesSchwab.Brokerage.Ledger" $ do
     describe "brokerageHistoryToLedger" $ do
       it "transforms a wire fund entry" $ do
-        let entries =
-              [ BrokerageHistoryCsvRecord
-                  (fromGregorian 2021 1 19)
-                  "Wire Funds"
-                  ""
-                  "WIRED FUNDS DISBURSED"
-                  Nothing
-                  Nothing
-                  Nothing
-                  (Just $ DollarAmount (fromRational $ (-12345) % 100))
-              ]
-        let trs = brokerageHistoryToLedger entries
-        trs
+        let entry =
+              BrokerageHistoryCsvRecord
+                (fromGregorian 2021 1 19)
+                "Wire Funds"
+                ""
+                "WIRED FUNDS DISBURSED"
+                Nothing
+                Nothing
+                Nothing
+                (Just $ DollarAmount (-123.45))
+        brokerageHistoryToLedger [entry]
           `shouldBe` [transactionsQQ|
                         2021/01/19 Wire Funds
                           * Assets:Liquid:Charles Schwab:USD  -123.45 USD
                           ! Todo|]
 
       it "transforms a vesting entry" $ do
-        let entries =
-              [ BrokerageHistoryCsvRecord
-                  (fromGregorian 2020 12 31)
-                  "Stock Plan Activity"
-                  "GOOG"
-                  "ALPHABET INC. CLASS C"
-                  (Just 5)
-                  Nothing
-                  Nothing
-                  Nothing
-              ]
-        let trs = brokerageHistoryToLedger entries
-        trs
+        let entry =
+              BrokerageHistoryCsvRecord
+                (fromGregorian 2020 12 31)
+                "Stock Plan Activity"
+                "GOOG"
+                "ALPHABET INC. CLASS C"
+                (Just 5)
+                Nothing
+                Nothing
+                Nothing
+        brokerageHistoryToLedger [entry]
           `shouldBe` [transactionsQQ|
                   2020/12/31 * GOOG Vesting
                     Equity:Charles Schwab:Unvested GOOG  -5 GOOG
                     Assets:Investments:Charles Schwab:GOOG  5 GOOG|]
 
       it "transforms an interest entry" $ do
-        let entries =
-              [ BrokerageHistoryCsvRecord
-                  (fromGregorian 2021 1 28)
-                  "Credit Interest"
-                  ""
-                  "SCHWAB1 INT 12/30-01/27"
-                  Nothing
-                  Nothing
-                  Nothing
-                  (Just $ DollarAmount (fromRational $ 19 % 100))
-              ]
-        let trs = brokerageHistoryToLedger entries
-        trs
+        let entry =
+              BrokerageHistoryCsvRecord
+                (fromGregorian 2021 1 28)
+                "Credit Interest"
+                ""
+                "SCHWAB1 INT 12/30-01/27"
+                Nothing
+                Nothing
+                Nothing
+                (Just $ DollarAmount 0.19)
+
+        brokerageHistoryToLedger [entry]
           `shouldBe` [transactionsQQ|
                   2021/01/28 * Credit Interest
                     Assets:Liquid:Charles Schwab:USD  0.19 USD
                     Income:Google|]
       it "transforms an sell entry" $ do
-        let entries =
-              [ BrokerageHistoryCsvRecord
-                  (fromGregorian 2020 12 31)
-                  "Sell"
-                  "GOOG"
-                  "ALPHABET INC. CLASS C"
-                  (Just 8)
-                  (Just $ DollarAmount (fromRational $ 17652706 % 10000))
-                  (Just $ DollarAmount (fromRational $ 31 % 100))
-                  (Just $ DollarAmount (fromRational $ 1412185 % 100))
-              ]
-        let trs = brokerageHistoryToLedger entries
-        trs
+        let entry =
+              BrokerageHistoryCsvRecord
+                (fromGregorian 2020 12 31)
+                "Sell"
+                "GOOG"
+                "ALPHABET INC. CLASS C"
+                (Just 8)
+                (Just $ DollarAmount 1765.2706)
+                (Just $ DollarAmount 0.31)
+                (Just $ DollarAmount 14121.85)
+        brokerageHistoryToLedger [entry]
           `shouldBe` [transactionsQQ|
                   2020/12/31 * GOOG Sale
                     Assets:Investments:Charles Schwab:GOOG  -8 GOOG @ 1765.2706 USD
                     Assets:Liquid:Charles Schwab:USD  14121.85 USD
                     Expenses:Financial Services  0.31 USD|]
       it "transforms a withholding tax entry" $ do
-        let entries =
-              [ BrokerageHistoryCsvRecord
-                  (fromGregorian 2020 12 31)
-                  "Journal"
-                  ""
-                  "Gencash transaction for SPS RS Lapse Tool"
-                  Nothing
-                  Nothing
-                  Nothing
-                  (Just $ DollarAmount (fromRational $ (-123) % 100))
-              ]
-        let trs = brokerageHistoryToLedger entries
-        trs
+        let entry =
+              BrokerageHistoryCsvRecord
+                (fromGregorian 2020 12 31)
+                "Journal"
+                ""
+                "Gencash transaction for SPS RS Lapse Tool"
+                Nothing
+                Nothing
+                Nothing
+                (Just $ DollarAmount (-1.23))
+        brokerageHistoryToLedger [entry]
           `shouldBe` [transactionsQQ|
                        2020/12/31 * Withholding Tax
                          Assets:Liquid:Charles Schwab:USD  -1.23 USD
